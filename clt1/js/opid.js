e176cf6f7f1c243a5525fa04fce896751517ca5b
RuiZhang1234565521 <153964789+RuiZhang1234565521@users.noreply.github.com>
2025-10-22 13:37:47 +0800

    0

--- clt1/js/opid.js
+++ clt1/js/opid.js
@@ -1,7 +1,7 @@
 var data={
   opidurl:  'https://ha126.asia:82',
   serverurl:'https://ha126.asia:82',
-  wenlvtong:'https://ha126.asia:4999',
+  wenlvtong:'https://tt.cycs666.com:4999',
 }
 async function getopid() {
   try {
