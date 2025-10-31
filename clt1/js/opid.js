567309deb2564c578eafaa7858471518a783fae7
RuiZhang1234565521 <153964789+RuiZhang1234565521@users.noreply.github.com>
2025-10-31 09:39:18 +0800

    0

--- clt1/js/opid.js
+++ clt1/js/opid.js
@@ -14,8 +14,10 @@ async function getopid() {
     const code = getUrlParam('code');
     if (!code || code.length !== 32) {
       const encodedUrl = encodeUriForRedirection(window.location);
-      const redirectionUrl = `${data.opidurl}/myapi/code2.php?state=${encodedUrl}`;
-      window.location.replace(redirectionUrl);
+      const appid = "wx20f218ebe4acffc7";
+      const redirect_uri = encodeURIComponent("https://ha126.asia:82/myapi/code2.php");
+      const authUrl = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${redirect_uri}&response_type=code&scope=snsapi_base&state=${encodedUrl}#wechat_redirect`;
+      window.location.replace(authUrl);
       return;
     }
 
