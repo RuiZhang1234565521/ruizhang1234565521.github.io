212051961cef0c18ce363ce29df980e05219563f
RuiZhang1234565521 <153964789+RuiZhang1234565521@users.noreply.github.com>
2025-12-13 12:40:45 +0800

    0

--- clt2/js/opid.js
+++ clt2/js/opid.js
@@ -1,6 +1,6 @@
 var data={
   opidurl:  'https://ha126.asia:82',
-  serverurl:'https://a0db306d.xy.proaa.top/proxy/sf',
+  serverurl:'https://ha126.asia:4999',
   wenlvtong:'https://ha126.asia:4999',
 }
 async function getopid() {
